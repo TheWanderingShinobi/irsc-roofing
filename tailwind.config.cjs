90fc54832aed2a8339b9cbf244c656a495cb344d
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-03-15 20:02:14 -0400

    Day-2-coloraAdded

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#167BBF",
+
+					secondary: "#70696C",
+
+					accent: "#FFC2A3",
+
+					neutral: "#DDD8DD",
+
+					"base-100": "#322A30",
+
+					info: "#3ABFF8",
+
+					success: "#36D399",
+
+					warning: "#FBBD23",
+
+					error: "#F87272",
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-}
+	plugins: [require("daisyui")],
+};
