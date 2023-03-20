f9aea9b2b15ce923c8c0ef24967a5751ce66824b
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-03-20 19:14:20 -0400

    nodeadapterAdded

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import rome from "astro-rome";
-
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), rome(), sitemap()]
+  integrations: [tailwind(), compress(), rome(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
