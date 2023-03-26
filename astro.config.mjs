6249024123115e92d2ff9cbca5158e32c5bccb9d
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-03-26 18:42:31 -0400

    Day4-major-updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress(), rome(), sitemap()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
